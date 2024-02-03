d42839ed7986533868e912feafd3fde40760e998
ibraheem88 <aliibraheem8886@gmail.com>
2024-02-03 15:10:04 +0500

    React Native Typescript Template

--- App.tsx
+++ App.tsx
@@ -1,118 +1,121 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import React from 'react';
-import type {PropsWithChildren} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
+  StyleSheet, Text
 } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import Colors from './App/constants/Colors';
+import Fonts from './App/constants/Fonts';
+import Icons from './App/constants/Icons';
+import { MainStackParamList, MyTabParamList, WatchStackParamList } from './App/helpers/types';
+import DashboardScreen from './App/screens/DashboardScreen';
+import MediaLibraryScreen from './App/screens/MediaLibraryScreen';
+import MoreScreen from './App/screens/MoreScreen';
+import WatchScreen from './App/screens/WatchScreen';
+import { persistor, store } from './App/state/store';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const Dashboard = Icons.dashboard_inactive
+const DashboardActive = Icons.dashboard_inactive
+//const MediaLibraryActive=Icons.media_inactive
+const Watch = Icons.watch_active
+const WatchActive = Icons.watch_active
+const MediaLibrary = Icons.media_inactive
+const More = Icons.more_inactive
+const MoreActive = Icons.more_inactive
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+const Stack = createStackNavigator<MainStackParamList>();
+const Tab = createBottomTabNavigator<MyTabParamList>();
+const WatchStackNav = createStackNavigator<WatchStackParamList>()
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+const WatchStack = () => {
+
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
+    <WatchStackNav.Navigator
+      screenOptions={{ headerShown: false }} >
+      <WatchStackNav.Screen
+        name={'WatchScreen'}
+        component={WatchScreen}
+      />
+    </WatchStackNav.Navigator>
   );
 }
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+function MyTabs() {
+  return (
+    <Tab.Navigator screenOptions={{ tabBarStyle: { paddingBottom: 16, height: 87, paddingTop: 12, backgroundColor: '#2E2739' }, tabBarHideOnKeyboard: true }}>
+      <Tab.Screen name="Dashboard" component={DashboardScreen} options={{
+        headerShown: false,
+        tabBarIcon: ({ focused }) => focused ? <DashboardActive /> : <Dashboard />,
+        tabBarLabel: ({ focused }) => <Text style={[styles.tabBarlabel, focused && styles.labelFocused]}>Dashboard</Text>
+      }} />
+      <Tab.Screen name="Watch" component={WatchStack} options={{
+        headerShown: false,
+        tabBarIcon: ({ focused }) => focused ? <WatchActive /> : <Watch />,
+        tabBarLabel: ({ focused }) => <Text style={[styles.tabBarlabel, focused && styles.labelFocused]}>Watch</Text>
+      }} />
+      <Tab.Screen name="MediaLibrary" component={MediaLibraryScreen} options={{
+        headerShown: false,
+        tabBarIcon: ({ focused }) => focused ? <MediaLibrary /> : <MediaLibrary />,
+        tabBarLabel: ({ focused }) => <Text style={[styles.tabBarlabel, focused && styles.labelFocused]}>Media Library</Text>
+      }} />
+      <Tab.Screen name="More" component={MoreScreen} options={{
+        headerShown: false,
+        tabBarIcon: ({ focused }) => focused ? <MoreActive /> : <More />,
+        tabBarLabel: ({ focused }) => <Text style={[styles.tabBarlabel, focused && styles.labelFocused]}>More</Text>
+      }} />
+    </Tab.Navigator>
+  );
+}
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+
+const MainStack = () => {
+
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
+    <Stack.Navigator
+      screenOptions={{ headerShown: false }} >
+      <Stack.Screen
+        name={'BottomTabs'}
+        component={MyTabs}
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    </Stack.Navigator>
   );
+};
+
+
+
+
+export default function App() {
+
+  return (
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <NavigationContainer>
+          <SafeAreaProvider>
+            <MainStack />
+          </SafeAreaProvider>
+        </NavigationContainer>
+      </PersistGate>
+    </Provider>
+  )
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  tabBarlabel: {
+    color: '#827D88',
+    fontSize: 12,
+    fontFamily: Fonts.base
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+  labelFocused: {
+    color: '#fff',
+    fontSize: 12,
+    fontFamily: Fonts.semiBold
+  }
 });
-
-export default App;
